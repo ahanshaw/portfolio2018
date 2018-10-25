a5a63374ebe4cb392c965b15ee28860fd732cfb1
Angela Hanshaw <ahanshaw@elevatexd.com>
2018-10-24 20:23:40 -0500

    add GA, show otter animation on scroll

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import VueHead from 'vue-head'
 import VueScrollTo from 'vue-scrollto'
 import TweenMax from 'greensock'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
 
 export const bus = new Vue();
 
 Vue.config.productionTip = false
 Vue.use(VueHead)
 Vue.use(VueScrollTo)
+Vue.use(VueAnalytics, {
+    id: 'UA-59729316-2'
+})
 
 new Vue({
   render: h => h(App)
