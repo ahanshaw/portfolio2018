8b9b7fb7d2289596586ed52ddf464b87fa101d56
ahanshaw <ahanshaw@users.noreply.github.com>
2022-10-16 11:20:15 -0500

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,14 +3,14 @@
         <div class="about__container">
             <div class="about__item">
                 <h2>About This Site</h2>
-                <p>I created this site using Vue.js and Vue CLI. The work items are generated from JSON files to make adding new projects&nbsp;easy.</p>
+                <p>I created this site using Vue. The work items are generated from JSON files to make adding new projects&nbsp;easy.</p>
                 <p>Most animations use CSS transitions, except for my otter pal, who was created with an SVG and&nbsp;Greensock.</p>
                 <p>The font family is either Avenir Next Variable, if your browser supports variable fonts, or Open Sans, if it&nbsp;doesn&#8217;t.</p>
-                <p>Hosted by <a target="_blank" rel="noopener noreferrer" href="https://www.netlify.com/">Netlify</a> from my Github repo (CD for the&nbsp;win!).</p>
+                <p>Hosted by <a target="_blank" rel="noopener noreferrer" href="https://www.netlify.com/">Netlify</a>.</p>
             </div>
             <div class="about__item">
                 <h2>About Me</h2>
-                <p>I built my first website more than twenty years ago, and while I&#8217;ve also worked in web and print design, writing, and editing since then, HTML and CSS have always been my truest loves. (I&#8217;ve become rather fond of React and Vue, too.) I&#8217;m all about creating delightful user interfaces with a focus on accessibility and good&nbsp;UX.</p>
+                <p>While I&#8217;ve worked in web and print design, writing, and editing, front-end development with a focus on accessibility and good&nbsp;UX has always had my heart, and has been my main focus for the last eight years. Aside from the basics (HTML, CSS, and JavaScript), I have React and Vue in my toolbox, and I&#8217;ve created sites using Craft CMS, WordPress, Shopify, Opimizely/Episerver, and Umbraco.</p>
                 <p>I am currently a Senior Front-End Developer at <a target="_blank" rel="noopener noreferrer" href="https://adagetechnologies.com/">Adage Technologies</a>. Previously, I was a Front-End Engineer at <a target="_blank" rel="noopener noreferrer" href="https://cliquestudios.com/">Clique Studios</a> and a Senior Interactive Developer at <a target="_blank" rel="noopener noreferrer" href="https://www.elevatestudios.com">Elevate</a>. I was also a mentor for new front-end developers at Designation (now Flatiron School) UI/UX design bootcamp.</p>
                 <p>And yes, I really, really like both otters and spectacularly bad&nbsp;puns.</p>
             </div>
